d0f414024efc2a46be1f235d09f01378d630b371
bergony.bandeira <bergony.bandeira@quark.tec.br>
2023-06-07 21:11:54 -0300

    init

---
+++ src/main/java/com/bergony/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,34 @@
+package com.bergony.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return  "Coach: " + coachName + ", Team Name: " + teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDialyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDialyFortune(){
+        return "Today is your lucky day ";
+    }
+}
